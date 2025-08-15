0686764d3088d8491dbc4c72928577aec12ed993
Daniele Locatelli <daniele3.locatelli@mail.polimi.it>
2025-08-15 09:38:16 +0200

    First commit - upload initial files

---
+++ src/test/java/com/anki/anki/AnkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anki.anki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
